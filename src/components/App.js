870e0eef9cfed19707ad9940d16697c8fe15cd5d
Wonjae.kim <wonjae.kim@isana.net>
2019-02-02 20:11:42 +0900

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    state = {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="ui container" style={{ marginTop: "10px" }}>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default App;
