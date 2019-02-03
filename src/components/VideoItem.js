6efcd80b18afb2893d356189280566d5eb506687
rowaxl <rowaxl0@gmail.com>
2019-02-03 22:00:20 +0900

    fix video header

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -12,7 +12,7 @@ class VideoItem extends React.Component {
             <div className="item">
                 <img className="ui image" src={this.props.detail.snippet.thumbnails.medium.url} alt={this.props.detail.snippet.title} />
                 <div className="content">
-                    <a className="header">{this.props.detail.snippet.title}</a>
+                    <div className="header">{this.props.detail.snippet.title}</div>
                 </div>
             </div>
         )
